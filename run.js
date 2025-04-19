6382b90d7558242d812615c1c721f523a3b4acb2
aiuvcode <643008455@qq.com>
2025-04-19 14:37:35 +0800

    refactor: 重构项目结构并更新主页布局
    删除不再使用的页面组件和样式文件，将主页从多导航布局改为聊天界面布局。更新 `run.js` 文件，移除旧逻辑并添加新功能。

--- run.js
+++ run.js
@@ -1,12 +1,2 @@
-function handleOddEven(array) {
-    let lastElement = array.pop();
-	if (lastElement % 2 === 0 ) {
-		return array.unshift(lastElement)
-	} else {
-		return array.push(lastElement*2)
-	}
-}
-
-const array1 = [15,6,8,19];
-console.log(handleOddEven(array1));
-console.log(array1); 
+const fruits = ["apple","banana"];
+fruits.forEach((fruit) => { return console.log(fruit.toUpperCase()) })
